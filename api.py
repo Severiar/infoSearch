834bb16bd0f5a10aab8190b3d45aa3852c0bd113
Aleksander Levykin <s02210450@gse.cs.msu.ru>
2024-02-07 09:34:39 +0300

    Adding usage example (task 5)

--- api.py
+++ api.py
@@ -3,11 +3,13 @@ from engine import tokenizer, model, qdrant_client, average_pool
 from qdrant_client.http import models
 import torch.nn.functional as F
 import uvicorn
+from keys import COLLECTION_NAME
+import requests
 
 app = FastAPI()
 
-@app.get('/get_relevant_sentences')
-def get_relevant_sentences(query: str, sentences_number: int = 10, collection_name: str = "wiki collection"):
+@app.get('/get_relevant_sentences_by_query')
+def get_relevant_sentences_by_query(query: str, sentences_number: int = 10):
     """Получает строку-запрос и возвращает k наиболее релевантных предложений из базы данных"""
     batch_dict = tokenizer([query,], max_length=512, padding=True, 
                                 truncation=True, return_tensors='pt')
@@ -18,7 +20,7 @@ def get_relevant_sentences(query: str, sentences_number: int = 10, collection_na
     embedding = F.normalize(embeddings, p=2, dim=1)[0]
 
     closest_vectors = qdrant_client.search(
-        collection_name=collection_name,
+        collection_name=COLLECTION_NAME,
         search_params=models.SearchParams(hnsw_ef=128, exact=True),
         query_vector=embedding,
         limit=sentences_number,
